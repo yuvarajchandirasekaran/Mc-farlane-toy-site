fc6fa94e2006770cccccc385d587ef723ccdf934
yuvarajchandirasekaran <yuvarajcdckap@gmail.com>
2023-11-29 17:01:02 +0530

    js functionalities and nav bar hided content place changed into header

--- index.js
+++ index.js
@@ -1,68 +1,65 @@
+// Below code is for tab view humburg icon change into close icon
 let menuBtn = document.querySelector('.tab-menu')
 let tbContainer = document.querySelector('.nav-cont')
 menuBtn.addEventListener('click', () => {
     menuBtn.innerText = (menuBtn.innerText === 'menu') ? 'close' : 'menu';
     (menuBtn.innerText === 'menu') ? tbContainer.classList.remove('open') : tbContainer.classList.add('open');
-
 })
-
-// const mainContainer = document.querySelector('.main-container');
-// const hoverContainers = document.querySelectorAll('.hover-content');
-
-// mainContainer.addEventListener("mouseover", function (e) {
-//     const id = e.target.dataset.id;
-//     if (id) {
-//         const element = document.getElementById(id);
-//         element.classList.add("open");
-//     }
-// });
-
-//  js code for nav-content show when nav lnks hover
-
-const mainContainer = document.querySelector('.nav-links');
-const hoverContents = document.querySelectorAll('.hover-content');
-
-
-mainContainer.addEventListener("mouseover", function (e) {
-    const id = e.target.closest('.link').dataset.id;
-
-    if (id) {
-        for (const content of hoverContents) {
-            if (content.id === id) {
-                content.classList.add("open");
-            } else {
-                content.classList.remove("open");
-            }
-        }
-    }
-});
-
-mainContainer.addEventListener("mouseleave", function (e) {
-    for (const content of hoverContents) {
-        content.classList.remove("open");
-    }
-});
-
-
+// ----------------------------------------------
 // jquery code for nav-content show when nav lnks hover
-
-$(document).ready(function () {
-    $('.link').mouseenter(function() {
-        var id = $(this).attr('data-id');
-        $('#' + id).addClass('open');
-    });
-
-    $('.nav-links').mouseleave(function() {
-        $('.hover-content').removeClass('open');
-    });
+function handleHoverEffect(mediaQuery) {
+    if (mediaQuery.matches) {
+        // Handling hover effect for navigation links
+        $('.link').mouseenter(function () {
+            var id = $(this).attr('data-id');
+            $(this).addClass('link-hover-effect');
+            $('#' + id).addClass('open');
+        });
+        $('.link').mouseleave(function () {
+            var id = $(this).attr('data-id');
+            $('.hover-content').removeClass('open');
+            $(this).removeClass('link-hover-effect');
+        });
+        // Handling hover effect for content containers
+        $('.hover-content').mouseenter(function () {
+            var contentId = $(this).attr('id');
+            $('.link[data-id="' + contentId + '"]').addClass('link-hover-effect');
+        });
+        $('.hover-content').mouseleave(function () {
+            var contentId = $(this).attr('id');
+            $('.link[data-id="' + contentId + '"]').removeClass('link-hover-effect');
+        });
+    } else {
+        // Handle functionality for screen sizes that don't match the media query (if needed)
+        // For example, you might want to remove all event listeners or perform different actions
+    }
+}
+// Check the initial media query state on page load
+var initialMediaQuery = window.matchMedia("(min-width: 1024px)");
+handleHoverEffect(initialMediaQuery);
+// Listen for changes in the media query
+initialMediaQuery.addListener(function(mediaQuery) {
+    handleHoverEffect(mediaQuery);
 });
-
-
-let tabViewLink = document.querySelectorAll('.tab-view-links')
-
-for (let i = 0; i < tabViewLink.length; i++) {
-    tabViewLink[i].addEventListener('mouseover', (e)=>{
-        // tabViewLink[i].previousSibling.style.backgroundColor = 'block'
-        console.log(tabViewLink[i].previousSibling);
-    })
+// --------------------------------------------------
+// Tab view links open and close functionality
+let tabViewLinks = document.querySelectorAll('.tab-view-links');
+let links = document.querySelectorAll('.tab-active-link');
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', () => {
+        const isOpen = tabViewLinks[i].classList.contains('open');
+        // Close all tab-view-links
+        tabViewLinks.forEach((tab) => {
+            tab.classList.remove('open');
+        });
+        // Remove 'link-hover-effect' from all links
+        links.forEach((link) => {
+            link.classList.remove('link-hover-effect');
+        });
+        // Toggle the classes for the clicked link and its associated tab-view-links
+        if (!isOpen) {
+            links[i].classList.add('link-hover-effect');
+            tabViewLinks[i].classList.add('open');
+        }
+    });
 }
